2e9d7d8787f1fcc70a9014f8a97588a11a4a8ce9
CaoY123456 <caoy_6666@126.com>
2023-03-10 00:30:43 +0800

    卷饼项目立项

---
+++ src/main/java/com/mine/ts/TacoesShopApplication.java
@@ -0,0 +1,13 @@
+package com.mine.ts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoesShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoesShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mine/ts/TacoesShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mine.ts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoesShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

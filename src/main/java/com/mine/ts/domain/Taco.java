9e30a7f8d7e2e05cf26aa80c86f4e739b6de9908
CaoY123456 <caoy_6666@126.com>
2023-03-15 01:10:03 +0800

    多表操作初探

--- src/main/java/com/mine/ts/domain/Taco.java
+++ src/main/java/com/mine/ts/domain/Taco.java
@@ -12,24 +12,23 @@ import java.util.List;
  * @date 2022-12-16 1:46
  * @description 墨西哥卷饼类
  */
-@Getter
+@Data
+@ToString
 public class Taco {
 
-    @Setter
     private Long id;
 
-    @Setter
     private Date createdAt;
 
-    @Setter
     private String name;
+
+    private List<Ingredient> ingredients;
 
     public Taco() {
         createdAt = new Date();
         this.ingredients = new ArrayList<>();
     }
 
-    private List<Ingredient> ingredients;
 
     /**
      * 添加配料，注意只有在没有的基础上才添加
--- src/test/java/com/mine/ts/dao/TacoDAOTest.java
+++ src/test/java/com/mine/ts/dao/TacoDAOTest.java
@@ -1,5 +1,6 @@
 package com.mine.ts.dao;
 
+import com.mine.ts.domain.Ingredient;
 import com.mine.ts.domain.Taco;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,15 @@ public class TacoDAOTest {
     public void testInsert() {
         Taco taco = new Taco();
         taco.setName("墨西哥经典款卷饼");
+        taco.addIngredient(new Ingredient(1, "蒸制饼皮", Ingredient.Type.WRAP.name()));
+        taco.addIngredient(new Ingredient(5, "美国精品肥牛", Ingredient.Type.PROTEIN.name()));
+        taco.addIngredient(new Ingredient(10, "生菜", Ingredient.Type.VEGGIES.name()));
+        taco.addIngredient(new Ingredient(17, "玛斯卡彭奶酪", Ingredient.Type.CHEESE.name()));
+        taco.addIngredient(new Ingredient(22, "柱侯酱汁", Ingredient.Type.SAUCE.name()));
+
+//        System.out.println(taco);
+        int addCount = tacoDAO.add(taco);
+        System.out.println("新增卷饼个数：" + addCount);
+
     }
 }
